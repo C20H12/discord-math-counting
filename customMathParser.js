9f77cbbcfb7a398718fff8e92bfb38280738fbe7
untitledC20H12 <95670812+C20H12@users.noreply.github.com>
2023-03-03 21:54:30 -0700

    wrapper for the math parser that allows saving

--- customMathParser.js
+++ customMathParser.js
@@ -1,19 +1,87 @@
 const math = require("mathjs");
+const SimpleDb = require("./SimpleDb");
+const G_database = new SimpleDb("database.txt");
 
-class MathParser {
-  
+class CustomMathParser {
   #parser = math.parser();
-  constructor(profile) {
-    this.profile = profile;
+  #profile = null;
+  #MAX_DEFINE = 10;
+
+  async #_new(userId) {
+    const res = await G_database.popValue(userId);
+    if (res !== null) {
+      const definesArr = res.split(",");
+      for (const v of definesArr) {
+        this.#parser.evaluate(v);
+      }
+      this.#profile = { userId, defines: definesArr };
+    } else {
+      this.#profile = { userId, defines: [] };
+    }
+    return this
   }
-  evaluate(...args) {
-    const all = this.#parser.getAll()
-    Object.keys(all).forEach(k => console.log(k, math.string(all[k])))
-    return this.#parser.evaluate(...args)
+
+  constructor(userId) {
+    return this.#_new(userId);
   }
 
+  define(expression) {
+    if (this.#profile.defines.length > this.#MAX_DEFINE) {
+      return [false, "Max numbe of variables and functions reached"];
+    }
+    try {
+      this.#parser.evaluate(expression);
+    } catch (e) {
+      return [false, e.message];
+    }
+    this.#profile.defines.push(expression);
+    return [true, ""];
+  }
 
+  evaluate(expression) {
+    if (expression.includes("=")) {
+      return [false, "An assignment is not an expression"];
+    }
+    let res = 0;
+    try {
+      res = this.#parser.evaluate(expression);
+    } catch (e) {
+      return [false, e.message];
+    }
+    return [true, res];
+  }
 
+  remove(varName) {
+    const varIndex = this.#profile.defines.findIndex(varStr => varStr.startsWith(varName));
+    if (varIndex === -1) {
+      return [false, `No variable or function with name ${varName} found`];
+    }
+    this.#parser.remove(varName);
+    this.#profile.defines.splice(varIndex, 1);
+    return [true, ""];
+  }
+
+  getDefines() {
+    return this.#profile.defines;
+  }
+
+  saveProfile() {
+
+    const saveKey = this.#profile.userId;
+    const saveValue = this.#profile.defines.join(",");
+    const res = G_database.insertValue(saveKey, saveValue);
+    if (!res) {
+      throw Error("saving failed");
+    }
+  }
+
+  getId() {
+    return this.#profile.userId
+  }
+
+  logDatabase() {
+    console.log("cmp db " + this.#profile.userId, G_database.bufferKeys, G_database.bufferVals)
+  }
 }
 
-module.exports = MathParser
+module.exports = CustomMathParser;
